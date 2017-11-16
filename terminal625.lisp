fb4460a791849f9fd5605b50b99f040b76f9911f
terminal625 <zpersoncool@gmail.com>
2017-11-16 17:05:36 -0500

    yip

--- terminal625.lisp
+++ terminal625.lisp
@@ -7,3 +7,50 @@
 		    *load-truename*)))
     (make-pathname :host (pathname-host path)
 		   :directory (pathname-directory path))))
+
+(defun map-package (fun &optional (package *package*))
+  (let ((package (find-package package)))
+    (do-symbols (sym package)
+      (when (eq package
+		(symbol-package sym))
+	(funcall fun sym)))))
+
+(defun vars (&optional (fun #'print) (package *package*))
+  (map-package (lambda (x)
+		 (when (boundp x)
+		   (funcall fun x)))
+	       package))
+
+(defun funs (&optional (fun #'print) (package *package*))
+  (map-package (lambda (x)
+		 (when (fboundp x)
+		   (funcall fun x)))
+	       package))
+
+(defun repl (&optional (end :end))
+  (catch end
+    (loop
+       (progn
+;	 (princ (package-name *package*))
+;	 (write-char #\>)
+;	 (write-char #\Space)
+	 )
+       (force-output)
+       (let* ((form (read))
+	      (results
+	       (multiple-value-list
+		(eval form))))
+	 (setf - form)
+	 (shiftf /// // / results)
+	 (shiftf +++ ++ + -)
+	 (shiftf *** ** * (first results))
+
+	 (dolist (item results)
+	   (print item))
+
+	 (terpri)))))
+
+(defmacro etouq (form)
+  (let ((var (gensym)))
+    `(macrolet ((,var () ,form))
+       (,var))))

876c3b788cafc158b6c8c0f29294d0acc528c19b
terminal625 <zpersoncool@gmail.com>
2018-07-19 07:12:33 -0400

    huh

--- something.lisp
+++ something.lisp
@@ -21,35 +21,46 @@
 ;;;;or????: lists are circular
 ;;;;(a b c d) -> #0=(a . (b . (c . (d . #0#))))
 
-;;;;??? means "don't care"
+;;;;"???" and "..." mean "don't care"
 
 ;;;;on cons cells:
 ;;;;references to cons cells have a "direction".
 ;;;;the reference is flipped in place when "flip" executes.
 ;;;;you cannot tell whether or not a reference is flipped or not, it is relative.
-;;;;syntax? <relative-front relative-back} {relative-back relative-front> 
+;;;;syntax? <relative-front relative-back} {relative-back relative-front>
+
+;;;;there are no symbols.
 
 (cons (se1) (se2) next)
 
 ;;;;if arg1 and arg2 are the same reference and not flipped relative to each
 ;;;;other, execute next-same. otherwise execute next-different.
 (test (arg1) (arg2) next-same next-different)
-(move (dest) (src) next)
 
-;;;;flip a value in-place.
-(flip (val) next)
+;;;;copy car of source into the car of destination
+(move destination source next)
 
-(flip (<} ) ???) -> (flip ({>) ???)
+(move (...) (foo) ...) -> (move (foo) (foo) ...)
+
+;;;;flip value in-place.
+(flip (value) next)
+
+(flip (<... ...}) ...) -> (flip ({... ...>) ...)
+
+;;;;flip toggles the direction of the cell on each invocation.
 
 ;;;;on the necessity of "fork":
 ;;;;each instruction could potentially trigger more
-;;;;than one "next". but that would make it unorthogonal?
-(fork (next1 next2))
+;;;;than one "next". but that would make instructions unorthogonal?
+;;;;should variable length arguments be supported? but those can be implemented
+;;;;with the others?
+(fork next1 next2)
 
 ;;;;on the necessity of "halt":
 ;;;;an evaluator could just jump to a loop that does nothing forever
 ;;;;relative to the other evaluators.
-(halt)
+;;;;(halt)
+
 
 
 ;;;;could this lisp run in a limited environment? like dna?
